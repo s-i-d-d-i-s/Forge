c1ddbee1553b8d88b28de88ec62a398e6b1f9c71
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-03-09 16:12:15 +0100

    Refactor

--- forge-frontend/src/app/components/add-expense/add-expense.component.ts
+++ forge-frontend/src/app/components/add-expense/add-expense.component.ts
@@ -12,12 +12,8 @@ import { DatabaseService } from 'src/app/services/database.service';
 export class AddExpenseComponent implements OnInit {
 
   show_alert = false;
-  current_name = '';
-  current_account = '';
-  current_amount = 0;
-  current_amountType = '';
-  current_currencyType = '';
-  current_date = new Date(Date.now());
+  expense = <Expense>{};
+  viewing_currency = '';
   settings: Settings = new Settings();
   eur_accounts: Account[];
   inr_accounts: Account[];
@@ -27,24 +23,27 @@ export class AddExpenseComponent implements OnInit {
   constructor(private db: DatabaseService) {
     this.eur_accounts = [];
     this.inr_accounts = [];
-
+    
     this.amountTypes = [
       'Credit',
       'Debit'
     ]
-    this.current_amountType = this.amountTypes[1];
+    
     this.currencyTypes = [
       'INR',
       'EUR'
     ]
-    this.current_currencyType = this.getViewingCurrency()!;
+    this.expense.timestamp = new Date(Date.now());
+    this.expense.amountType = this.amountTypes[1];
+    this.expense.currency = this.currencyTypes[0];
+    this.viewing_currency = this.getViewingCurrency()!;
   }
 
   ngOnInit(): void {
     this.db.accounts.subscribe(
       (data) => {
         if (data.length > 0)
-          this.current_account = data[0].name;
+          this.expense.account = data[0].name;
         for (let account of data) {
           if (account.closed == false && account.currency == "INR") {
             this.inr_accounts.push(account);
@@ -62,21 +61,13 @@ export class AddExpenseComponent implements OnInit {
   }
 
   addExpense() {
-    var expense = <Expense>{};
-    expense.name = this.current_name;
-    expense.account = this.current_account;
-    expense.timestamp = this.current_date;
-    expense.amount = this.current_amount;
-    expense.amountType = this.current_amountType;
-    expense.currency = this.current_currencyType
-
-    this.db.addExpense(expense);
+    this.db.addExpense(this.expense);
     this.show_alert = true;
     setTimeout(() => {
       this.show_alert = false;
     }, 2000);
-    this.current_amount = 0;
-    this.current_name = '';
+    this.expense.amount = 0;
+    this.expense.name = '';
   }
 
   getViewingCurrency() {
