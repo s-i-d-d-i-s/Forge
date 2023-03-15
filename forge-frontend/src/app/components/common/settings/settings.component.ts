f11f9b802921f1684291d53d513862e17c551bd7
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-03-15 02:15:35 +0100

    Bug fixes

--- forge-frontend/src/app/components/common/settings/settings.component.ts
+++ forge-frontend/src/app/components/common/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Settings } from 'src/app/models/Settings.model';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  show_alert=false;
+  currentSettings: Settings;
+  current_viewing_currency: string;
+  currencies: string[];
+  constructor(private db:DatabaseService) {
+    this.currentSettings = new Settings();
+    this.currencies = [
+      'INR',
+      'EUR'
+    ];
+    this.current_viewing_currency = this.db.getViewingCurrency()!;
   }
 
+  ngOnInit(): void {
+    this.db.settings.subscribe(
+      (data) => {
+        if(data != null){
+          this.currentSettings = data;
+        }
+      }
+    )
+  }
+
+  updateSettings(){
+    this.db.addSettings(this.currentSettings);
+  }
+  
+  update_exchange_rate(){
+    this.db.addSettings(this.currentSettings);
+  }
+
+  update_viewing_currency(){
+      this.db.setViewingCurrency(this.current_viewing_currency);
+  }
 }
