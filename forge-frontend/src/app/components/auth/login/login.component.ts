4295c20d8acc0592b6ec4dabfd78426c124e5a8b
Salty-Bear <98378052+Salty-Bear@users.noreply.github.com>
2023-05-28 23:31:19 +0530

    fixed old data on login

--- forge-frontend/src/app/components/auth/login/login.component.ts
+++ forge-frontend/src/app/components/auth/login/login.component.ts
@@ -15,7 +15,9 @@ export class LoginComponent implements OnInit {
     this.auth.uid.subscribe(
       (data) => {
         if (data.length != 0) {
-          this.router.navigate(["/dashboard"]);
+          this.router.navigate(["/dashboard"]).then(() =>{
+            location.reload();
+          });
         }
       }
     )
