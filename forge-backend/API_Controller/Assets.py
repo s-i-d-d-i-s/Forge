18e8d2d4891ca8e01d8085796c9ca7c9bbdae175
Siddharth Singh <siddharthsingh3099@gmail.com>
2023-07-15 21:30:13 +0000

    get assets API

--- forge-backend/API_Controller/Assets.py
+++ forge-backend/API_Controller/Assets.py
@@ -13,9 +13,9 @@ class AssetService:
 
 		@app.route('/get-assets/<uid>/<auth_token>/<currency>')
 		def get_assets(uid, auth_token, currency):
-			stocks = self.db.get_stocks(uid,auth_token)
+			assets = self.db.get_assets(uid,auth_token)
 			response = []
-			for stock in stocks:	
-				stock['price'] = self.db.get_price(stock['name'], currency)
-				response.append(stock)
+			for asset in assets:	
+				asset['price'] = self.db.convert_money(asset['price'], asset['currency'], currency)
+				response.append(asset)
 			return json.dumps(response)
